c99804793d79f455cdd326312f8654f2cebe95c1
TorGj <82477510+TorGj@users.noreply.github.com>
2021-12-20 19:10:58 +0100

    ToDo: 1 - Skrelle bort mer søppeldata 2 - Sende data til database 3 - Hente data fra database 4 - Bruke riktig dag sine eurokurser 5 - Legge til visning i GUI 6 - Lage mulighet for visning av enkelt dag 7 - Lage mulighet for veksling mellom graf/søyle ekstra info på graf, og andre visningsparametre 8 - Lage mulighet for bruker å laste ned regneark 9 -

--- entsold.py
+++ entsold.py
@@ -54,20 +54,16 @@ def App_GUI():
 
 u_a = App_GUI()
 firstdate = getDateRangeFromWeek(u_a[1],u_a[2])
-
-
-# create a Twython object by passing the necessary secret passwords
 tokenkey = str(Twython(config.api_key)).replace('<Twython: ','').replace('>','')
-
 place = u_a[0]
 
 #naa = dt.datetime.now().strftime("%Y%m%d")
 # Fetch price of EUR in NOK
-def getentsoe(day):
+def getentsoe(day, s):
     url_entso = 'https://transparency.entsoe.eu/api?documentType=A44&in_Domain=' + place + '&out_Domain=' + place + '&periodStart=' + day + '0000&periodEnd=' + day + '2300&securityToken=' + tokenkey
     ele = str(requests.Session().get(url=url_entso).text)
     klipp = ele.split("<Period>")[1].split("</Period>")[0]
-    skriv_fil(day,klipp)
+    skriv_fil(day, klipp, s)
     return klipp
 
 def geteurnok():
@@ -78,22 +74,25 @@ def geteurnok():
     return eurprice
 
 
-def les_fil(yyyymmdd):
-    min_fil = open('%s.txt' % yyyymmdd, 'r')
+def les_fil(yyyymmdd, s):
+    yyyymmdds = yyyymmdd+s
+    min_fil = open('tgdata/%s.txt' % yyyymmdds, 'r')
     innhold = min_fil.read()
     min_fil.close()
-    print('Data for', yyyymmdd, 'var på lager :-D bruker disse')
+    print('Data for', yyyymmdds, 'var på lager :-D bruker disse')
     return innhold
 
-def fil_eksisterer(yyyymmdd):
-    if (os.path.isfile('%s.txt' % yyyymmdd))==True:
-        return les_fil(yyyymmdd)
+def fil_eksisterer(yyyymmdd, s):
+    yyyymmdds = yyyymmdd + s
+    if (os.path.isfile('tgdata/%s.txt' % yyyymmdds))==True:
+        return les_fil(yyyymmdd,s)
     else:
-        return getentsoe(yyyymmdd)
+        return getentsoe(yyyymmdd, s)
 
-def skriv_fil(yyyymmdd, data):
+def skriv_fil(yyyymmdd, data, s):
+    yyyymmdds = yyyymmdd + s
     # Skriver til en fil til, men med et mer standard format...
-    with open('%s.txt' % yyyymmdd, 'w') as filehandle:
+    with open('tgdata/%s.txt' % yyyymmdds, 'w') as filehandle:
         json.dump(data, filehandle)
 
 
@@ -103,7 +102,7 @@ def bygg_data(euro,epris):
     t = []
     p = []
     i = 1
-    while i < 24:
+    while i < 25:
         timenr = epris.split("<position>" + str(i))[1].split("</price.amount>")[0]
         timepris = float(timenr.split("<price.amount>")[1].split("</price.amount>")[0])
         p_vat = round(timepris * 1.25 * euro / 1000, 2)
@@ -139,25 +138,42 @@ def plotter(xer, yer, m, p_max):
 
 eur_rate = geteurnok()
 dato = []
+
+def what_area(place):
+    area = ['Oslo', 'Arendal', 'Ålesund', 'Narvik', 'Bergen']
+    if place == '10YNO-1--------2':
+        return area[0]
+    elif place == '10YNO-2--------T':
+        return area[1]
+    elif place == '10YNO-3--------J':
+        return area[2]
+    elif place == '10YNO-4--------9':
+        return area[3]
+    else:
+        return area[4]
+
+region = what_area(place)
+
+
 for n in range(0, 7):
     # print('n',n)
     day = str(firstdate[n])
-    print('Henter priser for dato:', day)
-    dagens_priser = fil_eksisterer(day) # sjekk om filer finnes, i såfall bruk dem.. Sjekk_om_fil(day)
+    print('Henter og lagrer priser for ' + region + ' dato:', day)
+    dagens_priser = fil_eksisterer(day, region) # sjekk om filer finnes, i såfall bruk dem.. Sjekk_om_fil(day)
     #print('Fikk tak i dagens_priser', dagens_priser)
     bygg_data(eur_rate, dagens_priser)
     dato.append(day)
-# Make canvas with settings
-figure(num=None, figsize=(10, 6))  # len(x)*7
+# Make canvas with settings (w,h) in inches
+figure(num=None, figsize=(16, 6))  # len(x)*7
 font = {'family': 'Times New Roman',
         'weight': 'bold',
         'size': 12}
 plt.rc('font', **font)
-plt.suptitle('Strømprisene i perioden: ' + str(dato[0]) + ' - ' + str(dato[6]))
+plt.suptitle('Strømprisene i perioden: ' + str(dato[0]) + ' - ' + str(dato[6]) + ' for '+ region)
 # testbilde = Image.open('entso.png')
 # Make one plot pr day
 for m in range(0, len(x)):
-    print('Lager søylediagram for dag:', m)
+    print('Lager søylediagram for dag:', m+1)
     plotter(x[m], y[m], m, p_max)
 # When the plot "plt" is finished constructed:
 plt.show()
