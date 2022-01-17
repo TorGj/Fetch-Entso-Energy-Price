511c6069c8c4ccfd340ac9cf32656600501b02e4
TorGj <82477510+TorGj@users.noreply.github.com>
2022-01-17 19:09:06 +0100

    ignoretest

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # .gitignore should include reference to config.py
-api_key = "API_KEY"
+api_key = "65d38477-fb71-4328-a1a1-7340ba39b2af"
 api_secret = "YOUR_SECRET"
 access_token = "YOUR_ACCESS_TOKEN"
 token_secret = "YOUR_TOKEN_SECRET"
